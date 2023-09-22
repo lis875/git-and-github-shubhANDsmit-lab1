d90673115996094b07a113ca3ca625f21d19159a
Smit Patel <spatel234@wisc.edu>
2023-09-22 18:51:44 -0500

    Edited beverage and added last verse to the song

--- bottles.py
+++ bottles.py
@@ -1,16 +1,23 @@
-beverage = "water"
-count = 2
+beverage = "Lemonade"
+count = 6
 
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", "water")
-print("Take one down, pass it around")
-count = count - 1
-print(count, "bottles of", beverage, "on the wall")
+for count in range(6,0,-1):
+   # count = count - 1
+    print(count, "bottles of", beverage, "on the wall")
+    print(count, "bottles of", beverage)
+    print("Take one down, pass it around")
+    count = count - 1
+    print(count, "bottles of", beverage, "on the wall")
+    print("")
 
-print("")
+    #print(count, "bottles of", beverage, "on the wall")
+    #print(count, "bottles of", beverage)
+    #print("Take one down, pass it around")
+    #count = count - 1
+    #print(count, "bottles of", beverage, "on the wall")
+    #print("")
 
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", beverage)
-print("Take one down, pass it around")
-count = count - 1
-print(count, "bottles of", beverage, "on the wall")
+print("No more bottles of " + beverage + " on the wall, no more bottles of " + beverage)
+print("There's nothing else to fall, because there's no more bottles of " + beverage + " on the wall.")
+
+
