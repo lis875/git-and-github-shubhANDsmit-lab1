10c43b05c46eb678ebe95f0562233ccb34a45401
Smit Patel <spatel234@wisc.edu>
2023-09-22 20:00:19 -0500

    Created file Challenge.py and edited bottles.py

--- Challenge.py
+++ Challenge.py
@@ -1,21 +1,19 @@
-# Exercise 1 to 9:
-
 beverage = "Lemonade"
 
 for count in range(100,1,-1):
     print(count, "bottles of", beverage, "on the wall")
     print(count, "bottles of", beverage)
-    if (count == 5):
-        print("If one of those bottles should happen to fall, " + str(count-1) + " bottles of " + beverage + " on the wall...")
-    else:    
+    if (count%2 == 0):
         print("Take one down, pass it around")
+    else:    
+        print("If one of those bottles should happen to fall, " + str(count-1) + " bottles of " + beverage + " on the wall...")
     count = count - 1
     print(count, "bottles of", beverage, "on the wall")
     print("")
 
 print(count, "bottle of", beverage, "on the wall")
 print(count, "bottle of", beverage)
-print("Take one down, pass it around")
+print("If one of those bottles should happen to fall, no more bottles of " + beverage + " on the wall...")
 print("")
 print("No more bottles of " + beverage + " on the wall, no more bottles of " + beverage)
 print("There's nothing else to fall, because there's no more bottles of " + beverage + " on the wall.")
